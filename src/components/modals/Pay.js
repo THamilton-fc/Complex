d87da1569c4b23c3267430093572dfe85275555e
THamilton-fc <Travis@findcommerce.co>
2023-04-19 09:33:00 -0700

    build pay modal

--- src/components/modals/Pay.js
+++ src/components/modals/Pay.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 
-import sampleImage from '../../assets/img/sample.png';
 import findLogo from '../../assets/img/FIndlogo 1.png';
 
-function PayModal ({ isCurrentModal, setCurrentModal }) {
+function PayModal ({ buyItems, addBuyItem, isCurrentModal, setCurrentModal }) {
     const nextStep = () => {
         isCurrentModal++;
         setCurrentModal(isCurrentModal);
@@ -11,23 +10,69 @@ function PayModal ({ isCurrentModal, setCurrentModal }) {
             top: 150,
             behavior: 'smooth'
         });
+        
+        addBuyItem([]);
     }
 
+    const [subTotal, setSubTotal] = useState(0);
+    useEffect(() => {
+        var sum = 0;
+        buyItems.map((buyItem) => (
+            sum += buyItem.price
+        ));
+        setSubTotal(sum);
+        console.log(sum);
+    }, [buyItems]);
+
     return (
-        <div className='font-sans w-[531px] h-[557px] rounded-2xl bg-white bg-[#FFFFFF] pt-8 px-10 shadow-[0_0_20px_rgba(0,0,0,0.1)]'>
+        <div className='font-mont w-[531px] h-auto rounded-2xl bg-white bg-[#FFFFFF] pt-8 px-10 shadow-[0_0_20px_rgba(0,0,0,0.1)]'>
             <div>
-                <p className='text-[20px] traking-[2px] pb-8'>Order Review</p>
-                <div className='flex border-b border-black pb-10'>
-                    <img className='w-[87px] h-[100px]' src={sampleImage} alt='' />
-                    <div className='text-[14px] tracking-[2px] px-[52px]'>
-                        <p className='pb-4'>Item</p>
-                        <p className='font-medium w-[180px]'>EKD Print Stretch Tulle Turtleneck Bodysuit (S)</p>
-                    </div>
-                    <div className='text-[14px] tracking-[2px]'>
-                        <p className='pb-4'>Total</p>
-                        <p className='font-medium'>$1,100.00</p>
-                    </div>
+                <p className='text-[20px] font-semibold traking-[2px] pb-8'>Order Review</p>
+                
+                <table>
+                    <tr className='font-mont text-[14px] leading-[17px] tracking-[3px]'>
+                        <td></td>
+                        <td>Item</td>
+                        <td className='pl-[82px]'>Price</td>
+                    </tr>
+                    {
+                        buyItems.map((buyItem) => (
+                            <tr className='font-mont text-[14px] leading-[18px]'>
+                                <td className='pr-[53px]'>
+                                    <img src={buyItem.image} alt='' width={88} />
+                                </td>
+                                <td className='w-[154px] tracking-[2px]'>{buyItem.name}({buyItem.size})</td>
+                                <td className='pl-[82px] tracking-[1px]'>${buyItem.price.toLocaleString("en-US", { style: "decimal", minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
+                            </tr>
+                        ))
+                    }
+                </table>
+
+                <div className='w-[451px] border border-[#000000]'></div>
+
+                <div className='pt-[24px] pb-[32px] flex justify-end font-mont text-[14px] leading-[18px] tracking-[2px]'>
+                    <table>
+                        <tr>
+                            <td className='pr-[24px]'>Subtotal</td>
+                            <td className='flex justify-end'>${subTotal.toLocaleString("en-US", { style: "decimal", minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
+                        </tr>
+                        <tr>
+                            <td className='pr-[24px]'>Shipping</td>
+                            <td className='flex justify-end'>$50.00</td>
+                        </tr>
+                        <tr>
+                            <td className='pr-[24px]'>Tax</td>
+                            <td className='flex justify-end'>$167.00</td>
+                        </tr>
+                        <tr className='font-semibold'>
+                            <td className='pr-[24px]'>Total</td>
+                            <td className='flex justify-end'>$2,073.00</td>
+                        </tr>
+                    </table>
                 </div>
+
+                <p className='text-[20px] font-semibold traking-[2px] pb-[20px]'>Shipping Information</p>
+
                 <div className='pt-8 text-[14px] tracking-[2px]'>
                     <table className='w-full'>
                         <tbody>
@@ -56,14 +101,15 @@ function PayModal ({ isCurrentModal, setCurrentModal }) {
                         </tbody>
                     </table>
                 </div>
+
                 <button 
-                    className='mt-10 w-[170px] h-9 rounded-lg bg-[#145CE7] text-[16px] text-white font-semibold'
+                    className='mt-10 mb-[76px] w-[170px] h-9 rounded-lg bg-[#145CE7] text-[16px] text-white font-semibold'
                     onClick={nextStep}    
                 >
                     PAY NOW
                 </button>
             </div>
-            <div className='flex justify-center items-center gap-x-[8px] float-right'>
+            <div className='flex justify-center items-center gap-x-[8px] float-right mt-[-40px]'>
                 <p className='text-[#767676] text-[12px]'>Powered by</p>
                 <img className='w-[51px] h-[20px]' src={findLogo} alt='' />
             </div>
