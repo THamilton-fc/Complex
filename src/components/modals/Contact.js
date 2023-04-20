aaaf6863e85a4a84132d76faad1b0ba26fe5f074
THamilton-fc <Travis@findcommerce.co>
2023-04-20 09:42:29 -0700

    complete auto fill

--- src/components/modals/Contact.js
+++ src/components/modals/Contact.js
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from 'react';
 
 import findLogo from '../../assets/img/FIndlogo 1.png';
 
-function ContactModal ({ buyItems, isCurrentModal, setCurrentModal }) {
+function ContactModal ({ buyItems, formData, setFormData, isCurrentModal, setCurrentModal }) {
     const [subTotal, setSubTotal] = useState(0);
     
     useEffect(() => {
@@ -21,7 +21,20 @@ function ContactModal ({ buyItems, isCurrentModal, setCurrentModal }) {
             top: 150,
             behavior: 'smooth'
         });
+        localStorage.setItem('savedData', JSON.stringify(showData));
     }
+
+    const [showData, setShowData] = useState();
+
+    const autoFill = () => {
+        setShowData(formData);
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setShowData((prevData) => ({ ...prevData,  [name]: value }));
+        setFormData((prevData) => ({ ...prevData,  [name]: value }));
+    };
 
     return (
         <div className='font-sans w-[547px] h-auto rounded-2xl bg-white bg-[#FFFFFF] pt-8 shadow-[0_0_20px_rgba(0,0,0,0.1)]'>
@@ -30,48 +43,48 @@ function ContactModal ({ buyItems, isCurrentModal, setCurrentModal }) {
                     <h1 className='text-[18px] tracking-[2px] font-semibold'>Contact Information</h1>
                     <div className='pt-4 flex flex-col gap-y-[8px]'>
                         <label className='text-[14px] tracking-[2px]'>Name</label>
-                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='name' />
+                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='name' name='name' autoComplete='name' value={showData?.name || ''} onChange={handleChange} onClick={autoFill} required />
                     </div>
                     <div className='pt-3 flex flex-col gap-y-[8px]'>
                         <label className='text-[14px] tracking-[2px]'>Email Address</label>
-                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='email' />
+                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='email' name='email' autoComplete='email' value={showData?.email || ''} onChange={handleChange} onClick={autoFill} required />
                     </div>
                     <h1 className='pt-6 text-[18px] tracking-[2px] font-semibold'>Shipping Address</h1>
                     <div className='pt-4 flex flex-col gap-y-[8px]'>
                         <label className='text-[14px] tracking-[2px]'>Street</label>
-                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='street' />
+                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='street' name='street' autoComplete='street' value={showData?.street || ''} onChange={handleChange} onClick={autoFill} required />
                     </div>
                     <div className='pt-4 flex flex-col gap-y-[8px]'>
                         <label className='text-[14px] tracking-[2px]'>Apt/Unit No. (optional)</label>
-                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='unitno' />
+                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='unitno' name='unitno' autoComplete='unitno' value={showData?.unitno || ''} onChange={handleChange} onClick={autoFill} required />
                     </div>
                     <div className='pt-4 flex flex-col gap-y-[8px]'>
                         <label className='text-[14px] tracking-[2px]'>City</label>
-                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='city' />
+                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='city' name='city' autoComplete='city' value={showData?.city || ''} onChange={handleChange} onClick={autoFill} required />
                     </div>
                     <div className='flex gap-x-[12px]'>
                         <div className='pt-4 flex flex-col gap-y-[8px]'>
                             <label className='text-[14px] tracking-[2px]'>State</label>
-                            <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[175px] rounded-lg h-[40px] px-4' type='text' id='state' />
+                            <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[175px] rounded-lg h-[40px] px-4' type='text' id='state' name='state' autoComplete='state' value={showData?.state || ''} onChange={handleChange} onClick={autoFill} required />
                         </div>
                         <div className='pt-4 flex flex-col gap-y-[8px]'>
                             <label className='text-[14px] tracking-[2px]'>Zip Code</label>
-                            <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[133px] rounded-lg h-[40px] px-4' type='text' id='zipcode' />
+                            <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[133px] rounded-lg h-[40px] px-4' type='text' id='zipcode' name='zipcode' autoComplete='zipcode' value={showData?.zipcode || ''} onChange={handleChange} onClick={autoFill} required />
                         </div>
                     </div>
                     <h1 className='pt-6 text-[18px] tracking-[2px] font-semibold'>Payment Method</h1>
                     <div className='pt-4 flex flex-col gap-y-[8px]'>
                         <label className='text-[14px] tracking-[2px]'>Card Number</label>
-                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='cardnum' />
+                        <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[320px] rounded-lg h-[40px] px-4' type='text' id='cardnum' name='cardnum' autoComplete='cardnum' value={showData?.cardnum || ''} onChange={handleChange} onClick={autoFill} required />
                     </div>
                     <div className='flex gap-x-[12px]'>
                         <div className='pt-4 flex flex-col gap-y-[8px]'>
                             <label className='text-[14px] tracking-[2px]'>Zip Code</label>
-                            <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[175px] rounded-lg h-[40px] px-4' type='text' id='zipcode' />
+                            <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[175px] rounded-lg h-[40px] px-4' type='text' id='zipcode' name='zipcode' autoComplete='zipcode' value={showData?.zipcode || ''} onChange={handleChange} onClick={autoFill} required />
                         </div>
                         <div className='pt-4 flex flex-col gap-y-[8px]'>
                             <label className='text-[14px] tracking-[2px]'>CVV</label>
-                            <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[133px] rounded-lg h-[40px] px-4' type='text' id='cvv' />
+                            <input className='text-[14px] tracking-[2px] font-medium bg-[#F5F5F5] w-[133px] rounded-lg h-[40px] px-4' type='text' id='cvv' name='cvv' autoComplete='cvv' value={showData?.cvv || ''} onChange={handleChange} onClick={autoFill} required />
                         </div>
                     </div>
                 </div>
